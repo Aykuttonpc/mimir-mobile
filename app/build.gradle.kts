f1bd3e9f94a70a7a1662c0eb70efa53ce75ba424
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2026-05-09 13:25:38 +0300

    chore: versionName 0.1.0 -> 0.2.0 (test build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aykutcincik.mimir"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
